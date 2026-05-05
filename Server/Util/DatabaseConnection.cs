ee57cad88e3298c8cd21e1e3d0a51291b5dcb264
Luisa Grigoriiciuc <c4fyssrzzd@privaterelay.appleid.com>
2026-05-05 20:59:55 +0300

    realize ThreadStatic was overengineered but keeping it anyway

--- Server/Util/DatabaseConnection.cs
+++ Server/Util/DatabaseConnection.cs
@@ -8,7 +8,9 @@ public class DatabaseConnection
 {
     private static readonly ILogger Logger = Log.ForContext<DatabaseConnection>();
     private static readonly string DbUrl;
- 
+    //this static thread is overengineered for this app - because we have a shared lock by reserve, unreserve (the only methods who imply transactions) BUT for further refactoring it's necessary
+    //in java I had a local thread on a Connection object and there it was necessary since methods implying a transaction should use a specific connection, and the ones that don't imply a transaction aren't locked so they can run in parallel with the locked ones
+    //otherwise, they can't conflict with methods that don't imply a transaction, even if they run in parallel because they receive a new connection and the value in the local thread remains null
     [ThreadStatic]
     private static SqliteTransaction? _activeTransaction;
     static DatabaseConnection()
